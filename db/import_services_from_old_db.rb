67f05019bf3b54f99f4beb32dc175e68ff09bcf5
Chris Talib <piks3l@mailoo.org>
2017-11-17 15:26:13 +0100

    import script for services works

--- db/import_services_from_old_db.rb
+++ db/import_services_from_old_db.rb
@@ -5,9 +5,24 @@ puts "Starting the loop and the import..."
 @arr_errors = []
 Dir[File.join(Rails.root,"old_db/services/*.json")].each do |json_file|
   hash = JSON.parse(File.read(json_file))
+
+  begin
+    unless (hash['fulltos']['terms']['url']).nil?
+      url = hash['fulltos']['terms']['url']
+    else
+      unless (hash['fulltos']['url']).nil?
+        url = hash['fulltos']['url']
+      else
+        url = "no url"
+      end
+    end
+  rescue
+    url = "no url"
+  end
+
   imported_service = Service.find_or_add(
     hash['name'],
-    hash['fulltos']['terms']['url'])
+    url)
   unless imported_service.valid?
     puts "### #{imported_service.name} not imported ! ###"
     @arr_errors << imported_service.name
